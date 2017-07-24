a4d21d25af113958d0ddf371ee5632dd541aeebc
Caoyabin <774895876@qq.com>
2017-07-24 23:43:44 +0800

    Add predicting method of classifier

---
+++ src/com/buaa/hospital/nomogram/Classfier.java
@@ -0,0 +1,19 @@
+package com.buaa.hospital.nomogram;
+
+import weka.classifiers.meta.Bagging;
+import weka.core.Instance;
+import weka.core.SerializationHelper;
+
+public class Classfier {
+	private static Bagging baggingClassifier;
+	public static void buildClassfier(){
+		
+	}
+	public static double predict(String modelPath,Instance instance) throws Exception{
+		if("".equals(modelPath)||(instance==null)){
+			throw new Exception("Path empty or instance null");
+		}
+		baggingClassifier = (Bagging)SerializationHelper.read(modelPath);  //get serialized classifier from a model path
+		return baggingClassifier.classifyInstance(instance);  //get result
+	}
+}
